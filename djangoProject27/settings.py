408ba50f412d1cb39e0e77de60350b13db29fce6
johngachara <gachara@4146c987a9a6>
2024-04-16 08:11:42 +0000

    pythonanywhere settings

--- djangoProject27/settings.py
+++ djangoProject27/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%7dtqs0j0v-w$p15i1!*#+3ep(%)c1nlw9_0pm--n4yy)2ag%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["gachara.pythonanywhere.com"]
 
 
 # Application definition
