3e134f2d2b8cd3c1425681edca0d12d7eebc5f3a
John Gachara <johngachara29@gmail.com>
2024-05-24 05:20:41 +0300

    finishing up

--- isp/urls.py
+++ isp/urls.py
@@ -20,8 +20,6 @@ urlpatterns = [
     path('enable_customer',enable_customer,name='enable_customer'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    # Add other URL patterns as needed
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
