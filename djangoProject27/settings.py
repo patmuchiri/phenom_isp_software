a6709933bfc912020cd409a1c1c4070023541cd2
John Gachara <johngachara29@gmail.com>
2024-05-24 06:50:43 +0300

    finishing up

--- djangoProject27/settings.py
+++ djangoProject27/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%7dtqs0j0v-w$p15i1!*#+3ep(%)c1nlw9_0pm--n4yy)2ag%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["isp.phenom-ventures.com", '127.0.0.1', 'localhost']
 
